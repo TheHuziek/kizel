34202eb3cde48f11d4fb88182bf796840777fb5a
TheHuziek <71230598+TheHuziek@users.noreply.github.com>
2022-02-26 19:46:18 -0700

    Add files via upload
    I was able to solve the input comparison problem

--- kizel/src/main.rs
+++ kizel/src/main.rs
@@ -1,28 +1,47 @@
 use std::io::stdin;
-fn main(){
-    println!("escribe multiplicacion: ");
-    let mut nu:String=String::new(); /*
-    println!("no of bytes read , {}", b1);*/
-    let _b2 =stdin().read_line(&mut nu).unwrap();
-    let xd:bool=nu.eq("multiplicacion");
+fn main() {
+    println!("escribe la operacion a realizar\nejemplo:\nsuma\nmultiplica");
+    let mut nu: String = String::new(); /*println!("no of bytes read , {}", b1);*/
+    stdin().read_line(&mut nu).unwrap();
+    let xd = String::from("multiplicacion\n");
+    let suma= String::from("suma\n");
+    if nu == xd {
+        /*println!("ahora escribe que numeros quieres multiplicar");
+        let mut num1 = String::new();
+        let mut num2 = String::new();
+        stdin().read_line(&mut num1).unwrap();
+        stdin().read_line(&mut num2).unwrap();
+        let x: u32 = num1.trim().parse().unwrap();
+        let y: u32 = num1.trim().parse().unwrap();*/
+        multi();
+    }else if nu==suma{
+        suman()
+    }else {
+        println!("wtf escribe multiplicacion por favor {}", xd);
+    }
+}
+fn suman(){
 
-    if xd == true{
+    println!("ahora escribe que numeros quieres sumar");
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+    stdin().read_line(&mut num1).unwrap();
+    stdin().read_line(&mut num2).unwrap();
+    let x: u32 = num1.trim().parse().unwrap();
+    let y: u32 = num1.trim().parse().unwrap();
+    let r= x+y;
+    println!("esta es el resultadode la suma: {}",r)
+}
+fn multi() {
 
-     println!("ahora escribe que numeros quieres multiplicar");
-    let mut num1= String::new();
-    let mut num2= String::new();
-
-    let _b3= stdin().read_line(&mut num1).unwrap();
-    let _b4 =stdin().read_line(&mut num2).unwrap();
-    let x:u32 = num1.trim().parse().unwrap();
-    let y:u32 = num1.trim().parse().unwrap();
-        multi(x,y);
-    } else {    
-            println!("wtf escribe multiplicacion por favor {}",xd);
-        }
- }
- fn multi(x:u32,y:u32){
-    let r=x*y;
-    println!("este es el resultado de tu multiplicacion{}",r);
+    println!("ahora escribe que numeros quieres multiplicar");
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+    stdin().read_line(&mut num1).unwrap();
+    stdin().read_line(&mut num2).unwrap();
+    let x: u32 = num1.trim().parse().unwrap();
+    let y: u32 = num1.trim().parse().unwrap();
+    let r = x * y;
+    println!("este es el resultado de tu multiplicacion = {}", r);
     return;
- }
+}
